2e295c17b03ae6b871323705c0bc2cb8b65b64dd
willmcroberts <williammcroberts15@gmail.com>
2025-04-13 18:39:32 +0000

    Added Deck.java, barely started it, have lots to work on

---
+++ Deck.java
@@ -0,0 +1,14 @@
+// Deck.java
+import java.util.*;
+
+public class Deck implements HasMenu {
+
+	ArrayList<Card> cards = new ArrayList<Card>();
+
+	public static void main(String[] args) {
+		Deck deck = new Deck();
+		deck.start();
+	} // End main
+
+
+} // End Deck class
